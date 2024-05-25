d811750968b431e1713cd93b23208c9cdcbcc363
Kalle Hahtela <khahtela@gmail.com>
2024-05-25 21:23:50 +0300

    added location feature that will move the map in your location.

--- FibasteOy/app/(modals)/map.tsx
+++ FibasteOy/app/(modals)/map.tsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MapView from "react-native-maps";
 import { StyleSheet, View } from "react-native";
+import * as Location from 'expo-location';
 
 const Map = () => {
+    const [location, setLocation] = useState(null);
+    const [errorMsg, setErrorMsg] = useState(null);
+    const mapRef = React.useRef(null);
+
+    useEffect(() => {
+        (async () => {
+            let { status } = await Location.requestForegroundPermissionsAsync();
+            if (status !== 'granted') {
+                setErrorMsg('Permission to access location was denied');
+                return;
+            }
+
+            let location = await Location.getCurrentPositionAsync({});
+            setLocation({
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+                latitudeDelta: 0.0922,
+                longitudeDelta: 0.0421,
+            });
+
+            if (mapRef.current) {
+                mapRef.current.animateToRegion({
+                    latitude: location.coords.latitude,
+                    longitude: location.coords.longitude,
+                    latitudeDelta: 0.0922,
+                    longitudeDelta: 0.0421,
+                }, 1000); // Duration in milliseconds
+            }
+        })();
+    }, []);
+
     return (
         <View style={styles.container}>
-            <MapView style={styles.map}/>
+            <MapView ref={mapRef} style={styles.map} />
         </View>
     );
-
-    {/* TODO: implement a location handler to showcase near by tasks inm your area */}
 }
 
 const styles = StyleSheet.create({
